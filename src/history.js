5433f6a3cfde9fd7786f48ee4c25e5f2ad944b5b
Masayuki Murata <sfcmurata@gmail.com>
2018-04-25 14:25:36 +0900

    first completion

--- src/history.js
+++ src/history.js
@@ -1,26 +1,35 @@
 const user = "pencilrocketman"
+const password = "hogehoge"
 //const endPoint = 'https://shareki.tokyo/'
-const endPoint = 'http://localhost:3000/'
+const endPoint = 'http://localhost:3000/api/history'
+
+let fillZero = (number) => {
+  return ('0' + number).slice(-2);
+}
+
+let d = new Date();
+let year = d.getFullYear();
+let month = fillZero(d.getMonth() +1);
+let date = fillZero(d.getDate());
+let hour = fillZero(d.getHours());
+let minute = fillZero(d.getMinutes());
+let second = fillZero(d.getSeconds());
+let time = year + '-' + month + '-' + date + ' ' + hour + ':' + minute + ':' + second;
 
 const onVisited = async (historyItem) => {
-  //console.log(historyItem.url);
-  //console.log(historyItem.title);
-  //const data = [
-  //  ['title', historyItem.title],
-  //  ['url', historyItem.url]
-  //]
   const res = await window.fetch(endPoint, {
     method: 'POST',
-    //body: data.map((a) => encodeURIComponent(a[0]) + '=' + encodeURIComponent(a[1])).join('&'),
-    body: JSON.stringify({
-      name: user, 
-      title: historyItem.title,
-      url: historyItem.url
+    body: JSON.stringify({ 
+      history: {
+        name: user, 
+        password: password,
+        url: historyItem.url,
+        time: time 
+      }
     }), 
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
-      //'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
     }
   })
 }
